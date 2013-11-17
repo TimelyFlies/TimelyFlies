ea8d342dededcde1e9bf9a5c11ef4e4714480f37
TimelyFlies <timelyflies@gmail.com>
2013-11-17 15:59:21 -0700

    Update upload.php

--- TimelyFlies/upload.php
+++ TimelyFlies/upload.php
@@ -1,10 +1,17 @@
 <?php
-require_once 'header.php';
+  require_once 'header.php';
 
-  echo "<form action='uploader.php' method='post'";
-  echo "enctype='multipart/form-data'>";
-  echo "<label for='file'>Filename:</label>";
-  echo "<input type='file' name='file' id='file'><br>";
-  echo "<input type='submit' name='submit' value='Submit'>";
-  echo "</form>";
+  if($loggedin)
+  {
+    echo "<form action='uploader.php' method='post'";
+    echo "enctype='multipart/form-data'>";
+    echo "<label for='file'>Filename:</label>";
+    echo "<input type='file' name='file' id='file'><br>";
+    echo "<input type='submit' name='submit' value='Submit'>";
+    echo "</form>";
+  }
+  else
+  {
+    die("You must be logged in to view this page.");
+  }
 ?>
