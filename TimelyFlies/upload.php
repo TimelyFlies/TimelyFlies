9102317ac87d3ffdf6cb4afda1226592fefe152a
TimelyFlies <timelyflies@gmail.com>
2013-11-17 16:07:52 -0700

    Update upload.php

--- TimelyFlies/upload.php
+++ TimelyFlies/upload.php
@@ -6,12 +6,12 @@
 	echo "<div id='container'><div id='header' class='header'><h1>File Uploader</h1></div>";
 	echo "<h4>You can use this tool to upload files such as tickets or boarding passes</h4>";
 	echo "<div id='menu' class='menubar'>";
-    echo "<form action='uploader.php' method='post'";
-    echo "enctype='multipart/form-data'>";
-    echo "<label for='file'>Filename:</label>";
-    echo "<input type='file' name='file' id='file'><br>";
-    echo "<input type='submit' name='submit' value='Submit'>";
-    echo "</form>";
+	echo "<form action='uploader.php' method='post'";
+	echo "enctype='multipart/form-data'>";
+	echo "<label for='file'>Filename:</label>";
+	echo "<input type='file' name='file' id='file'><br>";
+	echo "<input type='submit' name='submit' value='Submit'>";
+	echo "</form>";
 	echo "</div>";
 	echo "</div>";
 
