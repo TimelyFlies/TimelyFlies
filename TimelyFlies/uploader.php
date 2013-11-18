5999c653f32f8ec6a98dd25187fefaaed01682fc
TimelyFlies <timelyflies@gmail.com>
2013-11-17 21:01:23 -0700

    Update uploader.php

--- TimelyFlies/uploader.php
+++ TimelyFlies/uploader.php
@@ -1,23 +1,35 @@
 <?php
-if ($_FILES["file"]["error"] > 0)
+if ($loggedin)
 {
-	echo "Error: " . $_FILES["file"]["error"] . "<br>";
-}
-else
-{
-	echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-	echo "Type: " . $_FILES["file"]["type"] . "<br>";
-	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-	echo "Temp file: " . $_FILES["file"]["tmp_name"];
-}
-if (file_exists("/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"]))
-{
-  echo $_FILES["file"]["name"] . " already exists. ";
-}
-else
-{
-  move_uploaded_file($_FILES["file"]["tmp_name"], "/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"]);
-  echo "<br> </br>";
-  echo "Stored in: " . "/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"];
+	if (isset($_POST['user']))
+	{
+	        $user = sanitizeString($_POST['user']);
+		echo "Test page for uploader";
+		if ($_FILES["file"]["error"] > 0)
+		{
+			echo "Error: " . $_FILES["file"]["error"] . "<br>";
+		}
+		else
+		{
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			echo "Temp file: " . $_FILES["file"]["tmp_name"];
+		}
+		if (file_exists("/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"]))
+		{
+		  echo $_FILES["file"]["name"] . " already exists. ";
+		}
+		else
+		{
+		  move_uploaded_file($_FILES["file"]["tmp_name"], "/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"]);
+		  echo "<br> </br>";
+		  echo "Stored in: " . "/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"];
+		}
+	}
+	else
+	{
+		echo "Error with user account";
+	}
 }
 ?>
