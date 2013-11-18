b1596cc440d5df7667012ccc648f99d463384a46
TimelyFlies <timelyflies@gmail.com>
2013-11-17 21:15:00 -0700

    Update uploader.php

--- TimelyFlies/uploader.php
+++ TimelyFlies/uploader.php
@@ -1,11 +1,16 @@
 <?php
+require_once 'header.php';
 echo "Test page for uploader";
+$path = "/opt/bitnami/apache2/htdocs/TimelyFlies";
 if ($loggedin)
 {
 	if (isset($_SESSION['user']))
 	{
         $user = sanitizeString($_SESSION['user']);
         echo "User: $user";
+        $path .= "/";
+        $path .= $user;
+        $path .= "/"
 		if ($_FILES["file"]["error"] > 0)
 		{
 			echo "Error: " . $_FILES["file"]["error"] . "<br>";
@@ -17,15 +22,15 @@ if ($loggedin)
 			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
 			echo "Temp file: " . $_FILES["file"]["tmp_name"];
 		}
-		if (file_exists("/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"]))
+		if (file_exists("$path" . $_FILES["file"]["name"]))
 		{
 		  echo $_FILES["file"]["name"] . " already exists. ";
 		}
 		else
 		{
-		  move_uploaded_file($_FILES["file"]["tmp_name"], "/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"]);
+		  move_uploaded_file($_FILES["file"]["tmp_name"], "$path" . $_FILES["file"]["name"]);
 		  echo "<br> </br>";
-		  echo "Stored in: " . "/opt/bitnami/apache2/htdocs/TimelyFlies/$user" . $_FILES["file"]["name"];
+		  echo "Stored in: " . "$path" . $_FILES["file"]["name"];
 		}
 	}
 	else
