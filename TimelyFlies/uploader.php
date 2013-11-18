7fbd6b1f554722253655af2d729c81c6ecef9daa
TimelyFlies <timelyflies@gmail.com>
2013-11-17 20:58:18 -0700

    Update uploader.php
    changed to test directory in david folder

--- TimelyFlies/uploader.php
+++ TimelyFlies/uploader.php
@@ -1,13 +1,23 @@
 <?php
- if ($_FILES["file"]["error"] > 0)
-  {
-    echo "Error: " . $_FILES["file"]["error"] . "<br>";
-  }
-  else
-  {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Stored in: " . $_FILES["file"]["tmp_name"];
-  }
+if ($_FILES["file"]["error"] > 0)
+{
+	echo "Error: " . $_FILES["file"]["error"] . "<br>";
+}
+else
+{
+	echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+	echo "Type: " . $_FILES["file"]["type"] . "<br>";
+	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+	echo "Temp file: " . $_FILES["file"]["tmp_name"];
+}
+if (file_exists("/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"]))
+{
+  echo $_FILES["file"]["name"] . " already exists. ";
+}
+else
+{
+  move_uploaded_file($_FILES["file"]["tmp_name"], "/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"]);
+  echo "<br> </br>";
+  echo "Stored in: " . "/opt/bitnami/apache2/htdocs/TimelyFlies/david/" . $_FILES["file"]["name"];
+}
 ?>
