016265df3feca7f2daf782bed2e56d982ac847e4
TimelyFlies <timelyflies@gmail.com>
2013-11-18 12:25:02 -0700

    Update browse.php

--- TimelyFlies/browse.php
+++ TimelyFlies/browse.php
@@ -22,13 +22,26 @@ if (isset($_POST['start'])) {
     $start = sanitizeString($_POST['start']);
     $flighttable = sanitizeString($_POST['flighttable']);
     $class = sanitizeString($_POST['class']);
+    
     $result = queryMysql("SELECT start, destination, date, time, $class FROM $flighttable WHERE start='$start'");
+    
     $rows = mysql_num_rows($result);
     if ($rows > 0) {
         echo "<table><thead><th>Start</th><th>Destination</th><th>Date</th><th>Time</th><th>Price</th></thead><tbody>";
         for ($j = 0; $j < $rows; ++$j) {
             $row = mysql_fetch_row($result);
-            echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$$row[4]</td></tr>";
+            $date = $row[2];
+            $destination = $row[1];
+            $time = $row[3];
+            echo "<tr><form name='bookflight' action='ticketing.php' method='POST'>" .
+                "<input type='hidden' name='start' value='$start'/>" .
+                "<input type='hidden' name='flighttable' value='$flighttable'/>" .
+                "<input type='hidden' name='class' value='$class'/>" .
+                "<input type='hidden' name='destination' value='$destination'/>" .
+                "<input type='hidden' name='date' value='$date'/>" .
+                "<input type='hidden' name='time' value='$time'/>" .
+                "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$$row[4]</td>" .
+                "<td><input type='submit' value='Tickets'/></td></form></tr>";
         }
         echo "</tbody></table>";
     } else {
