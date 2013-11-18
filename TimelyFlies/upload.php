04b0c5ec2a43295dd50755ea43ff5cbc7b8195f3
TimelyFlies <timelyflies@gmail.com>
2013-11-17 22:16:10 -0700

    Added <body> and <html> closing tags and self-closing <input> tags

--- TimelyFlies/upload.php
+++ TimelyFlies/upload.php
@@ -7,18 +7,18 @@
 	echo "<h4>You can use this tool to upload files such as tickets or boarding passes</h4>";
 	echo "<div id='menu' class='menubar'>";
 	echo "<form action='uploader.php' method='post'";
-    	echo "enctype='multipart/form-data'>";
-    	echo "<label for='file'>Filename:</label>";
-    	echo "<input type='file' name='file' id='file'><br>";
-    	echo "<input type='submit' name='submit' value='Upload'>";
-    	echo "</form>";
+        echo "enctype='multipart/form-data'>";
+        echo "<label for='file'>Filename:</label>";
+        echo "<input type='file' name='file' id='file'/><br>";
+        echo "<input type='submit' name='submit' value='Upload'/>";
+        echo "</form>";
 	echo "</div>";
 	echo "</div>";
 	echo "<div id='flights' class='main'>";
 	echo "<h4>Your Files:</h4>";
 	echo "List user's files here";
 	echo "</div>";
-
+    echo "</body></html>";
   }
   else
   {
