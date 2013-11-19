011f08c7333b39b55dcbaa45c1769a279d116760
TimelyFlies <timelyflies@gmail.com>
2013-11-18 21:01:29 -0700

    Update viewfile.php

--- TimelyFlies/viewfile.php
+++ TimelyFlies/viewfile.php
@@ -1,3 +1,23 @@
 <?php
+$file = $_GET['file'];
+echo "<script src='PDF.js'></script>";
 
+PDFJS.getDocument('$file').then(function(pdf)
+{
+  pdf.getPage(1).then(function(page)
+  {
+    var scale = 1.5;
+    var viewport = page.getViewport(scale);
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    var renderContext =
+    {
+      canvasContext: context,
+      viewport: viewport
+    };
+    page.render(renderContext);
+  };
+}
 ?>
