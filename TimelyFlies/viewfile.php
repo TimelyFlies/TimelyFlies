9ebca7e6aa0d38b8f32bcc16385da0940ac67e7f
TimelyFlies <timelyflies@gmail.com>
2013-11-19 19:46:43 -0700

    Change hard-coded user to $user, added a login check

--- TimelyFlies/viewfile.php
+++ TimelyFlies/viewfile.php
@@ -1,31 +1,33 @@
 <?php
 require_once 'header.php';
 //echo "Testing page version 3";
-$user = $_SESSION['user'];
-$path = "/opt/bitnami/apache2/htdocs/TimelyFlies/" . $user;
-$path = $path . "/";
-$filename = $_GET['file'];
-$filepath = $path . $filename;
-//echo "$filename";
-$extension = pathinfo($filepath, PATHINFO_EXTENSION);
 
-if ($extension == 'pdf')
-{
-    header('Content-type: application/pdf');
-    header('Content-Disposition: inline; filename='.basename($filepath));
-    header('Content-Description: View PDF');
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($filepath));
-    ob_clean();
-    flush();
-    readfile($filepath);
-    exit;
-}
-else
-{
-    echo "<img src='david/$filename'>$filename</img>";
-}
+    if ($loggedin) {
+        $user = $_SESSION['user'];
+        $path = "/opt/bitnami/apache2/htdocs/TimelyFlies/" . $user;
+        $path = $path . "/";
+        $filename = $_GET['file'];
+        $filepath = $path . $filename;
+        //echo "$filename";
+        $extension = pathinfo($filepath, PATHINFO_EXTENSION);
+
+        if ($extension == 'pdf') {
+            header('Content-type: application/pdf');
+            header('Content-Disposition: inline; filename=' . basename($filepath));
+            header('Content-Description: View PDF');
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filepath));
+            ob_clean();
+            flush();
+            readfile($filepath);
+            exit ;
+        } else {
+            echo "<img src='$user/$filename'>$filename</img>";
+        }
+    } else {
+        echo "Please <a href='login.php'>log in</a> to view your documents";
+    }
 ?>
