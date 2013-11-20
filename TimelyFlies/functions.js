ccc259681c396529d6cc7cd2ee933e9fe89f7dc5
TimelyFlies <timelyflies@gmail.com>
2013-11-19 21:27:36 -0700

    JavaScript missing semicolon

--- TimelyFlies/functions.js
+++ TimelyFlies/functions.js
@@ -94,6 +94,6 @@ function checkPasswords() {
     if (pass1 == pass2) {
         O('passwordinfo').innerHTML = "<span><font color='green'>&nbsp;&#x2714; Passwords match</font></span>";
     } else {
-        O('passwordinfo').innerHTML = "<span><font color='red'>&nbsp;&#x2718; Passwords don't match</font></span>"
+        O('passwordinfo').innerHTML = "<span><font color='red'>&nbsp;&#x2718; Passwords don't match</font></span>";
     }
 }
